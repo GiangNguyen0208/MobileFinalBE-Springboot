b17d8b5bbae1c27e9ea9dc11faa541bcc6c950db
MinhThong551 <21130551@st.hcmuaf.edu.vn>
2024-12-20 14:48:23 +0700

    fix

---
+++ src/main/java/com/example/mobile/mapper/IVoucherMapper.java
@@ -0,0 +1,16 @@
+package com.example.mobile.mapper;
+
+import com.example.mobile.dto.request.ShopCreationReq;
+import com.example.mobile.dto.request.ShopUpdateReq;
+import com.example.mobile.dto.response.ShopResponse;
+import com.example.mobile.entity.Shop;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+
+public interface IVoucherMapper {
+    Shop toShop(ShopCreationReq req);
+    ShopResponse toShopResponse(Shop shop);
+    void updateShop(@MappingTarget Shop shop, ShopUpdateReq req);
+}
